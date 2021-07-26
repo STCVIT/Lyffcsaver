c0f784000228fec3b133e1fee790f6e11f9059ca
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2021-07-26 15:59:45 +0530

    Animate while changing view

--- src/components/ReserveSlots.js
+++ src/components/ReserveSlots.js
@@ -3,8 +3,12 @@ import ReserveSlotsBubbles from "./ReserveSlotsBubbles";
 import styles from "../css/ReserveSlots.module.css";
 const ReserveSlots = ({ reservedSlots, toggleReserve, view }) => {
   return (
-    <div className={styles.container}>
-      {view === 0 ? (
+    <div
+      className={`${styles.container} ${
+        view === 0 ? styles.container1 : styles.container2
+      }`}
+    >
+      {/* {view === 0 ? (
         <ReserveSlotsTable
           reservedSlots={reservedSlots}
           toggleReserve={toggleReserve}
@@ -14,7 +18,28 @@ const ReserveSlots = ({ reservedSlots, toggleReserve, view }) => {
           reservedSlots={reservedSlots}
           toggleReserve={toggleReserve}
         ></ReserveSlotsBubbles>
-      )}
+      )} */}
+
+      <div
+        className={`${styles.wrapper1} ${
+          view === 0 ? styles.inactive : styles.active
+        }`}
+      >
+        <ReserveSlotsTable
+          reservedSlots={reservedSlots}
+          toggleReserve={toggleReserve}
+        ></ReserveSlotsTable>
+      </div>
+      <div
+        className={`${styles.wrapper2} ${
+          view === 1 ? styles.inactive : styles.active
+        }`}
+      >
+        <ReserveSlotsBubbles
+          reservedSlots={reservedSlots}
+          toggleReserve={toggleReserve}
+        ></ReserveSlotsBubbles>
+      </div>
     </div>
   );
 };
